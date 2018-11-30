613b318d769811f1d9a0af13e67a6dfffdcce100
roishmueli1 <33421651+roishmueli1@users.noreply.github.com>
2018-11-30 22:08:03 +0200

    another version to build the json file

--- sel2.py
+++ sel2.py
@@ -8,8 +8,6 @@ browser = webdriver.Chrome('C:\\Users\\Roi Shmueli\\Downloads\\chromedriver_win3
 browser.get('https://www.facebook.com/ShenkarConfessions/')
 # assert 'Yahoo' in browser.title
 
-#elem = browser.find_element_by_name('p')  # Find the search box
-#print(elem)
 #elem.send_keys('seleniumhq' + Keys.RETURN)
 SCROLL_PAUSE_TIME = 0.5
 
@@ -33,25 +31,13 @@ while True:
 	if(i>10):
 		break
 	
-# element1 = browser.find_elements_by_id('js_54')  # Find the search box
-# element2 = browser.find_elements_by_xpath("//p")
-# element3 = browser.find_elements_by_class_name('UFILikeSentenceText')
-# element4 = browser.find_elements_by_class_name('z_c3pyo1brp')  # Find the search box
-
-
-
-
-
-# for i in element1:
-# 	with open('yair.txt', 'a', encoding='utf-8') as f:
-# 		print(i.text, file=f)
-
 element = browser.find_elements_by_class_name('userContentWrapper')
-
 posts = []
+
 for i in element:
 	element1 = i.find_element_by_class_name('timestampContent')  # Find the search box
 	element2 = i.find_element_by_tag_name('p')
+
 	try:
 		post = {}
 		post["time"] = element1.text
@@ -60,36 +46,11 @@ for i in element:
 
 	except AttributeError:
 		pass
+
 print(posts)
 with open('data1.json', 'w', encoding='utf-8') as outfile:
     data = json.dump(posts, outfile, ensure_ascii=False)
 
-# for i in element3:
-
-# 	try:
-# 		post = {}
-# 		post["like"] = i.text
-
-# 		posts.append(post)
-
-# 	except AttributeError:
-# 		pass
-# print(posts)
-# with open('data1.json', 'w', encoding='utf-8') as outfile:
-#     data = json.dump(posts, outfile, ensure_ascii=False)
-    
-	# with open('yair11.txt', 'a', encoding='utf-8') as f:
-	# 	print(i.text, file=f) 
-
-# for i in element3:
-# 	span = i.find_element_by_tag_name("span")
-# 	with open('yair11.txt', 'a', encoding='utf-8') as f:
-# 		print(span.text, file=f)
-		
-# for i in element4:
-# 	with open('yair11.txt', 'a', encoding='utf-8') as f:
-# 		print(i.text, file=f) 
-		
 # browser.quit()
 # file.close() 
 

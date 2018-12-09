6ff21065b253a3c658a6e4d968bc96c375803c3a
yshur <yair.shur@gmail.com>
2018-12-10 01:05:51 +0200

    update crawler with hide text and with full time

--- selCrawler.py
+++ selCrawler.py
@@ -3,12 +3,15 @@ import time
 import os
 from selenium.webdriver.common.keys import Keys
 import json
+import re
 
 
 def getConfessions(confessionsSource):
+	print(confessionsSource)
+	
 	browser.get('https://www.facebook.com/'+confessionsSource+'/')
 
-	SCROLL_PAUSE_TIME = 0.5
+	SCROLL_PAUSE_TIME = 0.6
 	
 	# Get scroll height
 	last_height = browser.execute_script("return document.body.scrollHeight")
@@ -26,14 +29,28 @@ def getConfessions(confessionsSource):
 		# if new_height == last_height:  break
 		last_height = new_height
 		i = i+1
-		if(i>3):
+		if(i>400):
 			break
 		
 	element = browser.find_elements_by_class_name('userContentWrapper')
 
 	for i in element:
-		time1 = i.find_element_by_class_name('timestampContent')
-		time1 = time1.text
+		time1 = i.find_element_by_class_name('_5ptz')
+		time2 = time1.get_attribute("title")
+		time3 = re.split(' |\/|:', time2)
+		if time3[4][-2:] == 'am':
+			if time3[3] == '12':
+				time3[3] = "00"
+			if len(time3[3]) < 2:
+				time3[3] = '0'+time3[3]
+		else:
+			if int(time3[3]) < 12:
+				time3[3] = str(int(time3[3])+12)
+		time3[4] = time3[4][:-2]
+		time2 = time3[2]+'-'+time3[0]+'-'+time3[1]+' '+time3[3]+':'+time3[4]+":00"
+		print(time2)
+		
+		utime = time1.get_attribute("data-utime")
 		
 		contents = i.find_elements_by_tag_name('p')
 		if len(contents) == 0:
@@ -49,27 +66,42 @@ def getConfessions(confessionsSource):
 		
 		likes = i.find_elements_by_class_name('UFILikeSentenceText')
 		if len(likes) == 0:
-			likes = '0'
+			likes = 0
 		else:
 			likes = likes[0].find_element_by_tag_name('span')
 			likes = likes.text
+			likesNum = [int(s) for s in likes.split() if s.isdigit()]
+			if len(likesNum) > 0:
+				likesNum = likesNum[0]
+			else:
+				likesNum = 0
+			likesArr = likes.split(',')
+			likes = likesNum+len(likesArr)
 		
 		shares = i.find_elements_by_class_name('UFIShareLink')
 		if len(shares) == 0:
-			shares = '0'
+			shares = 0
 		else:
-			shares = shares[0].text	
+			shares = shares[0].text
+			shares = [int(s) for s in shares.split() if s.isdigit()]
+			shares = shares[0]
 			
 		comments = i.find_elements_by_class_name('UFIPagerLink')
 		if len(comments) == 0:
-			comments = '0'
+			comments = 0
 		else:
-			comments = comments[0].text		
+			comments = comments[0].text
+			comments = [int(s) for s in comments.split() if s.isdigit()]
+			if len(comments) > 0:
+				comments = comments[0]
+			else:
+				comments = 1
 		
 		try:
 			post = {}
 			post["source"] = confessionsSource
-			post["time"] = time1
+			post["time"] = time2
+			post["unix-time"] = utime
 			post["content"] = content
 			post["likes"] = likes
 			post["shares"] = shares

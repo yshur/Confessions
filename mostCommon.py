b30f7e5da8ac8f9b4c3650c80ecd3a165bb959b6
roishmueli1 <33421651+roishmueli1@users.noreply.github.com>
2018-12-28 00:20:33 +0200

    top10 words

---
+++ mostCommon.py
@@ -0,0 +1,23 @@
+import json
+import itertools
+from collections import Counter
+
+
+
+file = open('data100.json', 'r', encoding="utf8")
+python_obj = json.load(file)
+
+words_in_one_list = []
+
+for line in python_obj:
+	words = line['content'].split()
+	#print(words)
+	words_in_one_list.extend(words)
+#print(words_in_one_list)
+c = Counter(words_in_one_list)
+print(c)
+top10 = c.most_common(10)
+
+with open('top10.json', 'w', encoding='utf-8') as outfile:
+	json.dump(top10,outfile,indent=2, ensure_ascii=False)
+			
